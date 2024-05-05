79e1c467df245d617d5bb84137e03edf437a3b6a
Shikha <shikha.py36@gmail.com>
2024-05-05 18:26:54 +0530

    final finish

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -10,5 +10,5 @@ export const personalData = {
   twitter: 'https://x.com/ShikhaPy36?t=D79o2TEFOoC113zAPyGvxA&s=09',
   leetcode: "https://leetcode.com/u/shikha_pandey/",
   devUsername: "pandeyshikha075",
-  resume: ""
+  resume: "https://drive.google.com/file/d/14kk4F77tsd_-QqhZzk1kuzrPR62uN1hE/view?usp=sharing"
 }
