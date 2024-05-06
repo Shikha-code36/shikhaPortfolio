29ec59d8a65611bd9aae44aef59a59bbd16f63ec
Shikha <shikha.py36@gmail.com>
2024-05-06 16:34:09 +0530

    skill section changes

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -161,20 +161,24 @@ function HeroSection() {
                   hireable:
                 </span>
                 <span className="text-orange-400">lambda</span>
-                <span className="text-gray-400">{" ("}</span>
+                <span className="text-gray-400">{" hirefor: ("}</span>
               </div>
               <div>
-                <span className="ml-12 lg:ml-24 text-cyan-400">self.</span>
-                <span className="mr-2 text-white">hardWorker</span>
+                <span className="ml-12 lg:ml-24 text-cyan-400">hirefor</span>
+                <span className="mr-2 text-white">['hardWorker']</span>
                 <span className="text-amber-300">and</span>
               </div>
               <div>
-                <span className="ml-12 lg:ml-24 text-cyan-400">self.</span>
-                <span className="mr-2 text-white">problemSolver</span>
+                <span className="ml-12 lg:ml-24 text-cyan-400">hirefor</span>
+                <span className="mr-2 text-white">['quickLearner']</span>
                 <span className="text-amber-300">and</span>
               </div>
               <div>
-                <span className="ml-8 lg:ml-16 mr-2 text-gray-400">{')'}</span>
+                <span className="ml-12 lg:ml-24 text-cyan-400">hirefor</span>
+                <span className="mr-2 text-white">['problemSolver']</span>
+              </div>
+              <div>
+                <span className="ml-8 lg:ml-16 mr-2 text-gray-400">{")"}</span>
               </div>
               <div>
                 <span className="ml-4 lg:ml-8 text-gray-400">{`}`}</span>
